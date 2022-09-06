912bdd2a16578b874709bcd79f09ca8fecd686d8
XinweiDuan <98137945+XinweiDuan@users.noreply.github.com>
2022-09-07 00:10:27 +0800

    Store the xml file path and the sprite path

---
+++ Unity3d_code/PathDefine.cs
@@ -0,0 +1,26 @@
+/****************************************************
+    文件：PathDefine.cs
+    作者：XINWEI DUAN
+    邮箱: 418563981@qq.com
+    日期：#CreateTime#
+    功能：Nothing
+*****************************************************/
+
+using UnityEngine;
+
+public class PathDefine
+{
+    #region Configs
+    public const string MapComCfg = "Cfg/OtherDetection";
+    public const string AnimalComCfg = "Cfg/AnimalDetection";
+    #endregion
+
+    #region CompPrefabs
+    public const string ground = "Prefabs/Grass";
+    public const string tree = "Prefabs/Tree";
+    public const string house = "Prefabs/House";
+    public const string fence = "Prefabs/Fence";
+    public const string wolf = "Prefabs/Wolf";
+    public const string cow = "Prefabs/Cow";
+    #endregion
+};
